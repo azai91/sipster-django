3aaacaac1596c05f73ae349d18afb5a2497c99c2
Andrew Carlson <acarl005@g.ucla.edu>
2015-07-12 18:49:08 -0700

    set up users

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Bar(models.Model):
+  yelp_id = models.CharField(required=True)
+  review_count = models.IntegerField(default=0)
+  happy_times = models.CharField()
